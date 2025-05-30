5fca0fa7578272bb3a2aaf1b98cf0d13b3fad08f
ValkyrJS <valkyr.photos@gmail.com>
2025-05-30 16:41:33 +0100

    App output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/plugin/glicko/assets/app/",
+  build: {
+    emptyOutDir: true,
+    outDir: "./dist/app",
+  },
   plugins: [react()],
 });
