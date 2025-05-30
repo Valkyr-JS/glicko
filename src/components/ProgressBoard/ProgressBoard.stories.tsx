1b313db9e7b2e9d054889497dc70928a94d7641e
ValkyrJS <valkyr.photos@gmail.com>
2025-05-30 23:15:40 +0100

    Reverse stories

--- src/components/ProgressBoard/ProgressBoard.stories.tsx
+++ src/components/ProgressBoard/ProgressBoard.stories.tsx
@@ -11,7 +11,7 @@ const meta = {
       ["Mia", "Monique", 0],
       ["Jo", "Alyx", 1],
       ["Poppy", "Lotti", 1],
-      ["Jay", "sammy", 0],
+      ["Jay", "Sammy", 0],
       ["Anna", "Serena", 1],
       ["Danni", "Maddy", 0],
       ["Melody", "Kelly", 0],
@@ -27,11 +27,42 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  /** Table data should be displayed in ascending order (match 1 at the top) by
-   * default. */
-  play: async ({ canvasElement }) => {
+  /** Table data should be displayed in ascending order (first match at the top)
+   * by default. */
+  play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
-    const firstColumn = canvas.getAllByRole("cell");
-    expect(firstColumn[0].textContent).toBe("1");
+    const cells = canvas.getAllByRole("cell");
+    expect(cells[0].textContent).toBe("1");
+    expect(cells[1].textContent).toBe(args.tableData[0][0]);
+  },
+};
+
+export const NoReverse: Story = {
+  args: {
+    reverse: false,
+  },
+  /** Table data should be displayed in ascending order (first match at the
+   * top). */
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const cells = canvas.getAllByRole("cell");
+    expect(cells[0].textContent).toBe("1");
+    expect(cells[1].textContent).toBe(args.tableData[0][0]);
+  },
+};
+
+export const Reverse: Story = {
+  args: {
+    reverse: true,
+  },
+  /** Table data should be displayed in descending order (last match at the
+   * top). */
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const cells = canvas.getAllByRole("cell");
+    expect(cells[0].textContent).toBe("10");
+    expect(cells[1].textContent).toBe(
+      args.tableData[args.tableData.length - 1][0]
+    );
   },
 };
