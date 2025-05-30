2b1f95f0a70730482f60e4a404c18208b59e77c5
ValkyrJS <valkyr.photos@gmail.com>
2025-05-30 22:39:26 +0100

    Progress board copied from original repo

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,21 +1,30 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
+import { INITIAL_VIEWPORTS } from "storybook/viewport";
+import "../public/index-84976af5.css";
 
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    initialGlobals: {
+      viewport: { value: "iphone5", isRotated: false },
+    },
+    layout: "fullscreen",
+    viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
   },
+  tags: ["autodocs"],
 };
 
 export default preview;
---
+++ src/components/ProgressBoard/ProgressBoard.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import ProgressBoard from "./ProgressBoard";
+
+const meta = {
+  title: "Components/Progress board",
+  component: ProgressBoard,
+  args: {
+    columnTitles: ["A", "B"],
+    tableData: [
+      ["Lotti", "Sabine", 1],
+      ["Marina", "Sabine", 1],
+      ["Giselle", "Sabine", 0],
+      ["Giselle", "Aria", 0],
+      ["Giselle", "Sophie", 0],
+      ["Giselle", "Rhian", 1],
+      ["Kelly", "Rhian", 0],
+      ["Kelly", "Lizz", 1],
+      ["Sammy", "Lizz", 0],
+      ["Sammy", "Taylor", 0],
+    ],
+    title: "Progress board",
+  },
+} satisfies Meta<typeof ProgressBoard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
