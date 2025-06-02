54a573b7ce2ea10d5601a78676e560af0587425c
ValkyrJS <valkyr.photos@gmail.com>
2025-06-02 19:38:32 +0100

    Storybook FormSubmissionDecorator

---
+++ .storybook/decorators.tsx
@@ -0,0 +1,48 @@
+import type { ReactRenderer } from "@storybook/react-vite";
+import { useState } from "react";
+import type { DecoratorFunction } from "storybook/internal/csf";
+
+type Decorator =
+  | DecoratorFunction<
+      ReactRenderer,
+      {
+        [x: string]: unknown;
+      }
+    >
+  | DecoratorFunction<
+      ReactRenderer,
+      {
+        [x: string]: unknown;
+      }
+    >;
+
+/** Storybook decorator which wraps the story in a form with a submit button.
+ * The form response value is displayed in a code block beneath it. */
+export const FormSubmissionDecorator: Decorator = (Story) => {
+  const [response, setResponse] = useState<{
+    [k: string]: FormDataEntryValue;
+  }>({});
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const formJson = Object.fromEntries(formData.entries());
+    setResponse(formJson);
+  };
+
+  const responseEl = Object.keys(response).length ? (
+    <code className="d-block mt-3">{JSON.stringify(response)}</code>
+  ) : null;
+
+  return (
+    <div>
+      <form method="post" onSubmit={handleSubmit}>
+        {Story()}
+        <button type="submit" className="btn btn-primary mt-5">
+          Submit form
+        </button>
+      </form>
+      {responseEl}
+    </div>
+  );
+};
