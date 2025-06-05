6fe9aace6bd3e6cf7cfd093d7b13aefe3b65df08
ValkyrJS <valkyr.photos@gmail.com>
2025-06-05 11:20:48 +0100

    Mock player data

--- src/pages/Tournament/Tournament.stories.tsx
+++ src/pages/Tournament/Tournament.stories.tsx
@@ -1,12 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import TournamentPage from "./Tournament";
 import { WithMemoryRouter } from "../../../.storybook/decorators";
+import mockPerformers from "../../mocks/Performers.json";
+import { Glicko2 } from "glicko2";
+import { getStashContent } from "../../../.storybook/tools";
+
+const tournament = new Glicko2();
 
 const meta = {
   title: "Pages/Tournament",
   component: TournamentPage,
   decorators: [WithMemoryRouter],
   args: {
+    players: mockPerformers.map((p) => {
+      return {
+        ...p,
+        coverImg: getStashContent(p.coverImg),
+        glicko: tournament.makePlayer(1500),
+      };
+    }),
     matchIndex: 0,
   },
 } satisfies Meta<typeof TournamentPage>;
--- src/pages/Tournament/Tournament.tsx
+++ src/pages/Tournament/Tournament.tsx
@@ -1,8 +1,11 @@
+import type { PlayerData } from "@/types/global";
 import React from "react";
 
 interface TournamentPageProps {
   /** The zero-based index of the current match in the match list. */
   matchIndex: number;
+  /** The data for all players involved in the tournament. */
+  players: PlayerData[];
 }
 
 const TournamentPage: React.FC<TournamentPageProps> = (props) => {
