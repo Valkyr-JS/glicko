d997df878b15cfbb4a39f37e6d6ee9b6e2764d45
ValkyrJS <valkyr.photos@gmail.com>
2025-06-05 11:28:31 +0100

    Matchboard added to tournament

--- src/pages/Tournament/Tournament.tsx
+++ src/pages/Tournament/Tournament.tsx
@@ -1,5 +1,6 @@
-import type { PlayerData } from "@/types/global";
 import React from "react";
+import type { PlayerData } from "@/types/global";
+import MatchBoard from "@/components/MatchBoard/MatchBoard";
 
 interface TournamentPageProps {
   /** The zero-based index of the current match in the match list. */
@@ -10,7 +11,34 @@ interface TournamentPageProps {
 
 const TournamentPage: React.FC<TournamentPageProps> = (props) => {
   console.log(props);
-  return <main>Tournament page</main>;
+
+  /** Handler for clicking the change player image button. */
+  const changeImageHandler = () => console.log("changeImageHandler");
+  /** Handler for clicking the pause button. */
+  const clickPauseHandler = () => console.log("clickPauseHandler");
+  /** Executes when the user selects the winning player. */
+  const clickSelectHandler = () => console.log("clickSelectHandler");
+  /** Handler for clicking the skip button. */
+  const clickSkipHandler = () => console.log("clickSkipHandler");
+  /** Handler for clicking the stop button. */
+  const clickStopHandler = () => console.log("clickStopHandler");
+  /** Handler for clicking the undo button. */
+  const clickUndoHandler = () => console.log("clickUndoHandler");
+
+  return (
+    <main>
+      <MatchBoard
+        changeImageHandler={changeImageHandler}
+        clickPauseHandler={clickPauseHandler}
+        clickSelectHandler={clickSelectHandler}
+        clickSkipHandler={clickSkipHandler}
+        clickStopHandler={clickStopHandler}
+        clickUndoHandler={clickUndoHandler}
+        matchIndex={props.matchIndex}
+        players={[props.players[0], props.players[1]]}
+      />
+    </main>
+  );
 };
 
 export default TournamentPage;
