491216d006b151c23fc1803861003aa27c9cb60b
ValkyrJS <valkyr.photos@gmail.com>
2025-06-06 01:41:39 +0100

    AbandonMatchModal

--- src/pages/Tournament/Tournament.tsx
+++ src/pages/Tournament/Tournament.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import MatchBoard from "@/components/MatchBoard/MatchBoard";
 import ProgressBoard from "@/components/ProgressBoard";
 import { PATH } from "@/constants";
 import type { Match, PlayerData } from "@/types/global";
+import Modal from "@/components/Modal/Modal";
+import { faHand } from "@fortawesome/pro-solid-svg-icons";
 
 interface TournamentPageProps {
   /** Handler for declaring a match a draw. */
@@ -23,6 +25,7 @@ interface TournamentPageProps {
 }
 
 const TournamentPage: React.FC<TournamentPageProps> = (props) => {
+  const [showAbandonModal, setShowAbandonModal] = useState(false);
   const navigate = useNavigate();
 
   if (!props.matchList.length) return null;
@@ -36,8 +39,14 @@ const TournamentPage: React.FC<TournamentPageProps> = (props) => {
   /** Handler for clicking the pause button. */
   const clickPauseHandler = () => console.log("clickPauseHandler");
 
+  /** Handler for confirming abandonment of the current tournament. */
+  const handleAbandonTournament = () => setShowAbandonModal(true);
+
+  /** Handler for cancelling abandonment of the current tournament. */
+  const handleCancelAbandonTournament = () => setShowAbandonModal(false);
+
   /** Handler for clicking the stop button. */
-  const handleAbandonTournament = () => {
+  const handleConfirmAbandonTournament = () => {
     // Navigate home
     navigate(PATH.HOME);
 
@@ -46,30 +55,82 @@ const TournamentPage: React.FC<TournamentPageProps> = (props) => {
   };
 
   return (
-    <main>
-      <MatchBoard
-        changeImageHandler={changeImageHandler}
-        clickPauseHandler={clickPauseHandler}
-        clickSelectHandler={props.selectWinnerHandler}
-        clickSkipHandler={props.declareDrawHandler}
-        clickStopHandler={handleAbandonTournament}
-        clickUndoHandler={props.undoMatchHandler}
-        matchIndex={props.matchIndex}
-        players={[playerA, playerB]}
+    <>
+      <main>
+        <MatchBoard
+          changeImageHandler={changeImageHandler}
+          clickPauseHandler={clickPauseHandler}
+          clickSelectHandler={props.selectWinnerHandler}
+          clickSkipHandler={props.declareDrawHandler}
+          clickStopHandler={handleAbandonTournament}
+          clickUndoHandler={props.undoMatchHandler}
+          matchIndex={props.matchIndex}
+          players={[playerA, playerB]}
+        />
+        <ProgressBoard
+          columnTitles={["Performer A", "Performer B"]}
+          reverse
+          tableData={props.matchList
+            .filter((m) => m.length === 3)
+            .map((m) => [
+              props.players[m[0]].name,
+              props.players[m[1]].name,
+              m[2],
+            ])}
+        />
+      </main>
+      <AbandonMatchModal
+        closeHandler={handleCancelAbandonTournament}
+        continueHandler={handleConfirmAbandonTournament}
+        show={showAbandonModal}
       />
-      <ProgressBoard
-        columnTitles={["Performer A", "Performer B"]}
-        reverse
-        tableData={props.matchList
-          .filter((m) => m.length === 3)
-          .map((m) => [
-            props.players[m[0]].name,
-            props.players[m[1]].name,
-            m[2],
-          ])}
-      />
-    </main>
+    </>
   );
 };
 
 export default TournamentPage;
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                       Abandon match modal                                      */
+/* ---------------------------------------------------------------------------------------------- */
+
+interface AbandonMatchModalProps {
+  /** Handler for cancelling abandonment of the current tournament. */
+  closeHandler: () => void;
+  /** Handler for confirming abandonment of the current tournament. */
+  continueHandler: () => void;
+  /** Dictates whether the modal is active. */
+  show: boolean;
+}
+
+const AbandonMatchModal: React.FC<AbandonMatchModalProps> = (props) => {
+  return (
+    <Modal
+      buttons={[
+        {
+          element: "button",
+          children: "No",
+          className: "btn btn-secondary",
+          onClick: props.closeHandler,
+          type: "button",
+        },
+        {
+          element: "button",
+          className: "btn btn-danger",
+          children: "Yes",
+          type: "button",
+          onClick: props.continueHandler,
+        },
+      ]}
+      icon={faHand}
+      show={props.show}
+      title="Abandon tournament?"
+    >
+      <p>
+        If you abandon the tournament, all progress will be lost. This cannot be
+        undone.
+      </p>
+      <p>Are you sure you want to abandon the tournament?</p>
+    </Modal>
+  );
+};
