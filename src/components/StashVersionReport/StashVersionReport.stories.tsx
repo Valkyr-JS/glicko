fe8b7d2ad43d7a5c8530f8c6b07c46afff7a4225
ValkyrJS <valkyr.photos@gmail.com>
2025-06-08 12:11:55 +0100

    VersionReport connected

--- src/components/StashVersionReport/StashVersionReport.stories.tsx
+++ src/components/StashVersionReport/StashVersionReport.stories.tsx
@@ -1,12 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import StashVersionReport from "./StashVersionReport";
+import { expect, within } from "storybook/test";
 
 const meta = {
   title: "Components/Stash Version Report",
   component: StashVersionReport,
-  args: {
-    version: "v0.28.1",
-  },
   parameters: {
     layout: "padded",
   },
@@ -15,5 +13,25 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-/** Expect the undo button to be disabled on start, otherwise enabled. */
-export const FullyCompatible: Story = {};
+/** Connecting to Stash successful. */
+export const Connected: Story = {
+  args: {
+    request: {
+      loading: false,
+      data: {
+        version: {
+          version: "v0.28.1",
+          hash: "cc6917f2",
+          build_time: "2025-03-19 23:01:38",
+        },
+      },
+      error: undefined,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const connectStatusText =
+      canvas.getByText<HTMLElement>(/Connected to Stash/i);
+    expect(connectStatusText).toBeInTheDocument();
+  },
+};
--- src/components/StashVersionReport/StashVersionReport.tsx
+++ src/components/StashVersionReport/StashVersionReport.tsx
@@ -1,13 +1,28 @@
-import type { StashVersion } from "@/apollo/schema";
 import React from "react";
+import type { OperationVariables, QueryResult } from "@apollo/client";
+import type { StashVersion } from "@/apollo/schema";
+import styles from "./StashVersionReport.module.scss";
 
 interface StashVersionReportProps {
-  version: StashVersion["version"]["version"];
+  request: {
+    loading: boolean;
+    data: QueryResult<StashVersion, OperationVariables>["data"];
+    error: QueryResult<StashVersion, OperationVariables>["error"];
+  };
 }
 
 const StashVersionReport: React.FC<StashVersionReportProps> = (props) => {
   console.log(props);
-  return <div>Version report</div>;
+
+  const connectionSuccessful = !props.request.loading && !props.request.error;
+
+  const connectionMessage = connectionSuccessful ? (
+    <div>Connected to Stash</div>
+  ) : (
+    <div>Unable to connect to Stash. Please check your connection.</div>
+  );
+
+  return <div className={styles.VersionReport}>{connectionMessage}</div>;
 };
 
 export default StashVersionReport;
