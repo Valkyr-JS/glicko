ab959e5b5776202455bebd7604fc27dbdd86a44e
ValkyrJS <valkyr.photos@gmail.com>
2025-06-13 22:40:43 +0100

    fixed story

--- src/components/ProgressBoard/ProgressBoard.stories.tsx
+++ src/components/ProgressBoard/ProgressBoard.stories.tsx
@@ -3,69 +3,71 @@ import { expect, within } from "storybook/test";
 import ProgressBoard from "./ProgressBoard";
 import { DEFAULT_MAX_PROGRESS_BOARD_ROWS } from "@/constants";
 
+const performerData = [
+  {
+    id: 28,
+    name: "Sophie",
+  },
+  {
+    id: 38,
+    name: "Danielle",
+  },
+  {
+    id: 7,
+    name: "Lily",
+  },
+  {
+    id: 76,
+    name: "Mia",
+  },
+  {
+    id: 5,
+    name: "Sophia",
+  },
+  {
+    id: 6,
+    name: "Kelly",
+  },
+  {
+    id: 2,
+    name: "Hannah",
+  },
+  {
+    id: 71,
+    name: "Tilly",
+  },
+  {
+    id: 88,
+    name: "Claire",
+  },
+  {
+    id: 4,
+    name: "Susan",
+  },
+  {
+    id: 59,
+    name: "Molly",
+  },
+  {
+    id: 83,
+    name: "Giselle",
+  },
+  {
+    id: 100,
+    name: "Aubrey",
+  },
+  {
+    id: 77,
+    name: "Kate",
+  },
+];
+
 const meta = {
   title: "Components/Progress board",
   component: ProgressBoard,
   args: {
     columnTitles: ["A", "B"],
-    performerData: [
-      {
-        id: 28,
-        name: "Sophie",
-      },
-      {
-        id: 38,
-        name: "Danielle",
-      },
-      {
-        id: 7,
-        name: "Lily",
-      },
-      {
-        id: 76,
-        name: "Mia",
-      },
-      {
-        id: 5,
-        name: "Sophia",
-      },
-      {
-        id: 6,
-        name: "Kelly",
-      },
-      {
-        id: 2,
-        name: "Hannah",
-      },
-      {
-        id: 71,
-        name: "Tilly",
-      },
-      {
-        id: 88,
-        name: "Claire",
-      },
-      {
-        id: 4,
-        name: "Susan",
-      },
-      {
-        id: 59,
-        name: "Molly",
-      },
-      {
-        id: 83,
-        name: "Giselle",
-      },
-      {
-        id: 100,
-        name: "Aubrey",
-      },
-      {
-        id: 77,
-        name: "Kate",
-      },
-    ],
+    performerData,
     results: [
       [28, 38, 0],
       [7, 76, 1],
@@ -119,7 +121,7 @@ export const Reverse: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const cells = canvas.getAllByRole("cell");
-    expect(cells[0].textContent).toBe("10");
+    expect(cells[0].textContent).toBe((performerData.length / 2).toString());
   },
 };
 
