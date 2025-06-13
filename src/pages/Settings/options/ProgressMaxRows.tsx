3686796fcd6b429a63a0348e282b413d413c2b4c
ValkyrJS <valkyr.photos@gmail.com>
2025-06-13 22:19:18 +0100

    progressMaxRows defaults

--- src/pages/Settings/options/ProgressMaxRows.tsx
+++ src/pages/Settings/options/ProgressMaxRows.tsx
@@ -1,4 +1,5 @@
 import NumberInput from "@/components/forms/NumberInput/NumberInput";
+import { DEFAULT_MAX_PROGRESS_BOARD_ROWS } from "@/constants";
 import React from "react";
 
 interface ProgressMaxRowsProps {
@@ -9,7 +10,7 @@ const ProgressMaxRows: React.FC<ProgressMaxRowsProps> = (props) => {
   return (
     <NumberInput
       id="progressMaxRows"
-      initialValue={props.userMaxRows ?? 5}
+      initialValue={props.userMaxRows ?? DEFAULT_MAX_PROGRESS_BOARD_ROWS}
       label="Maximum number of recent matches shown"
       name="progress-max-rows"
       min={1}
