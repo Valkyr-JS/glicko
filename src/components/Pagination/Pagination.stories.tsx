b834b7b2ad121f39de92a1480c0e2241128d0181
ValkyrJS <valkyr.photos@gmail.com>
2025-06-25 14:43:16 +0100

    First page button

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,9 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Pagination from "./Pagination";
+import { expect, fn, within } from "storybook/test";
 
 const meta = {
   title: "Components/Pagination",
   component: Pagination,
+  args: {
+    setCurrent: fn(),
+  },
   parameters: {
     layout: "padded",
   },
@@ -12,9 +16,32 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Small: Story = {
+export const FirstPageActive: Story = {
   args: {
     count: 3,
     current: 1,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const firstBtn = canvas.getByRole<HTMLButtonElement>("button", {
+      name: "Load first page",
+    });
+
+    expect(firstBtn).toBeDisabled();
+  },
+};
+
+export const FirstPageNotActive: Story = {
+  args: {
+    count: 3,
+    current: 2,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const firstBtn = canvas.getByRole<HTMLButtonElement>("button", {
+      name: "Load first page",
+    });
+
+    expect(firstBtn).not.toBeDisabled();
   },
 };
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import { faChevronsLeft } from "@fortawesome/pro-solid-svg-icons/faChevronsLeft";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 interface PaginationProps {
@@ -5,11 +7,34 @@ interface PaginationProps {
   count: number;
   /** The current page number */
   current: number;
+  /** Set the new page number */
+  setCurrent: (newCurrent: number) => void;
 }
 
 const Pagination: React.FC<PaginationProps> = (props) => {
   console.log(props);
-  return <div>Pagination</div>;
+
+  /* -------------------------------------- First page button ------------------------------------- */
+
+  const firstButtonDisabled = props.current === 1;
+  const handleFirstButtonClick: React.MouseEventHandler = () =>
+    props.setCurrent(1);
+
+  /* ------------------------------------------ Component ----------------------------------------- */
+
+  return (
+    <div className="pagination btn-group">
+      <button
+        type="button"
+        className="btn btn-secondary"
+        disabled={firstButtonDisabled}
+        onClick={handleFirstButtonClick}
+      >
+        <span className="sr-only">Load first page</span>
+        <FontAwesomeIcon icon={faChevronsLeft} />
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
